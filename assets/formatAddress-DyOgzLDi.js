91fa3b1a73f20df37c29d59ed156c6d4992f37e7
web3nbdao <hotbilong@hotmail.com>
2026-06-10 12:17:17 +0800

    Add files via upload

---
+++ assets/formatAddress-DyOgzLDi.js
@@ -0,0 +1,1 @@
+function e(e,r=6,n=4){if(!e)return"";if(e.length<=r+n)return e;return`${e.slice(0,r)}...${e.slice(-n)}`}export{e as f};
---
+++ assets/use-id-Cf5Aydqx.js
@@ -0,0 +1,1 @@
+import{au as n}from"./index-6j5nEYpD.js";let o=0;function t(){const t=n(),{name:e="unknown"}=(null==t?void 0:t.type)||{};return`${e}-${++o}`}export{t as u};
